8991d3206967b23f38c0aa2fa37487d70fbfcc53
Leh Barbosa <contato.barbosaads@gmail.com>
2026-01-29 21:53:28 -0300

    feat: adicionado exercícios 036 e 037 de lógica com estruturas condicionais aninhadas

--- mundo-02/exercicios/ex036.py
+++ mundo-02/exercicios/ex036.py
@@ -5,3 +5,24 @@ O programa vai perguntar o valor da casa, o salário do comprador e em quantos a
 Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário
 ou então o empréstimo será negado.
 """
+from traceback import print_tb
+
+print('\033[33m-=\033[m'*20)
+print('{:^35}'.format('Bank LHB'))
+print('\033[33m-=\033[m'*20)
+
+valor_casa = float(input('Digite o valor da casa: R$ '))
+salario = float(input('Informe o salário do comprador: R$ '))
+anos = int(input('Em quantos anos o cliente prentende pagar? '))
+# Calculo da prestação:
+prestacao = valor_casa / (anos * 12)
+
+limite = salario * 0.3
+if prestacao > limite:
+    print('\033[33m-=\033[m' * 20)
+    print('\033[31mEmpréstimo NEGADO!\033[m')
+    print('A parcela é acima de 30% do salário. \nO valor da parcela seria R${:.2f}'.format(prestacao))
+else:
+    print('\033[33m-=\033[m' * 20)
+    print('Empréstimo \033[32mAPROVADO! Parabéns\033[m')
+    print('Valor da parcela ao mês R${:.2f}'.format(prestacao))
--- mundo-02/exercicios/ex037.py
+++ mundo-02/exercicios/ex037.py
@@ -6,3 +6,29 @@ qual será a base de conversão:
 > 2 para octal
 > 3 para hexadeciaml
 """
+from traceback import print_tb
+
+print('\033[33m-=\033[m'*20)
+print('{:^35}'.format('Conversão de base'))
+print('\033[33m-=\033[m'*20)
+
+num = int(input('Digite um valor para ver a base de conversão: '))
+opcao = int(input('''
+Escolha uma das opções abaixo:
+[ 1 ] para Base Binária
+[ 2 ] para Base Octal
+[ 3 ] para Base Hexadecimal
+Opção: ''' ))
+
+if opcao == 1:
+    binario = bin(num)
+    print('O valor {} convertido para Binário é: {}'.format(num, binario))
+elif opcao == 2:
+    octal = oct(num)
+    print('O valor {} convertido para Octal é: {}'.format(num, octal))
+elif opcao == 3:
+    hexa = hex(num)
+    print('O valor {} convertido para Hexadeciaml é: {}'.format(num, hexa))
+else:
+    print('Opção inválida!')
+print('FIM DO PROGRAMA!')
